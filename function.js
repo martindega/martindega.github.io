b185ba7f2a507a9e392eedadd9de901e7e078d35
martindega <127315083+martindega@users.noreply.github.com>
2023-03-08 10:23:44 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,5 @@
+document.getElementById ("boton"). onclick = function(){
+    console.log("Captura en el evento clik");
+    document.getElementById("demo").innerHTML ="Conocimientos: PHP, Symfony, Laravel, Yii, Codeignitier, React, React Native, Redux, Express.js, MySQL, ElasticSearch, JavaScript, GCP, AWS, EC2, S3, RDS, BigQuery, MongoDB, Twig, Yaml, GIT";
+};
+    
